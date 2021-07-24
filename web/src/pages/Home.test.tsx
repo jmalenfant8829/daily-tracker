0e1f3bee0225b9526829e62bd3be8cdab398b26e
julien <julienm8080@gmail.com>
2021-07-24 16:08:43 -0400

    fix querying table data

--- web/src/pages/Home.test.tsx
+++ web/src/pages/Home.test.tsx
@@ -1,10 +1,18 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AUTH_TOKEN } from '../constants';
-import { testToken } from '../mocks/handlers';
-import userEvent from '@testing-library/user-event';
+import { testToken, initMockStorage } from '../mocks/handlers';
 
 import Home from './Home';
+
+beforeEach(() => {
+  initMockStorage();
+});
+
+afterEach(() => {
+  sessionStorage.clear();
+});
 
 function renderHomePage() {
   localStorage.setItem(AUTH_TOKEN, testToken);
@@ -34,11 +42,12 @@ test('should render weekly time table', async () => {
 
 test('should save change made to time table cell', async () => {
   renderHomePage();
-  const inputCell = await screen.findByRole('cell', { name: /20/i });
-  userEvent.type(inputCell, '80');
+  const cell = await screen.findByRole('cell', { name: /20/i });
+  const cellInput = within(cell).getByDisplayValue(/20/i);
+  userEvent.type(cellInput, '80');
   const saveButton = screen.getByRole('button', { name: /save changes/i });
   userEvent.click(saveButton);
   renderHomePage();
-  const editedCell = await screen.findByRole('cell', { name: /80/i });
+  const editedCell = await screen.findByRole('cell', { name: /2080/i });
   expect(editedCell).toBeInTheDocument();
 });
